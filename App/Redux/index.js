a5a5b04f7dc8866dfed9e85b9a277930d516e611
Chris Ball <chris@echobind.com>
2017-02-20 17:00:35 -0500

    Add basic redux persist

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,9 +1,29 @@
-import { combineReducers, createStore } from 'redux'
+import {
+  combineReducers,
+  createStore,
+  compose,
+  applyMiddleware
+} from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
+import { AsyncStorage } from 'react-native'
 import settings from './Modules/Settings/reducer'
 import timer from './Modules/Timer/reducer'
 
+const REDUX_PERSIST_OPTIONS = {
+  storage: AsyncStorage
+}
+
 const reducers = combineReducers({ settings, timer })
-const store = createStore(reducers)
+
+const store = createStore(
+  reducers,
+  undefined,
+  compose(
+    autoRehydrate()
+  )
+)
+
+persistStore(store, REDUX_PERSIST_OPTIONS)
 
 // testing
 // const unsubscribe = store.subscribe(() =>
